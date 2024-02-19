6e277ebeb51c9dd48e0408c36fec5528940ba837
Chidera JohnClinton Ezeoke <jclinton162@gmail.com>
2024-02-19 13:30:52 +0100

    New classes

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ class Platform {
   constructor(x, y) {
     this.position = {
       x,
-      y,
+      y
     };
     this.width = 200;
     this.height = 40;
@@ -65,7 +65,15 @@ class Platform {
     ctx.fillStyle = "#acd157";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
-}
+};
+
+class CheckPoint {
+  constructor(x, y) {
+    this.position = {
+      x,y
+    }
+  };
+};
 
 const player = new Player();
 
@@ -92,6 +100,11 @@ const platforms = platformPositions.map(
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  platforms.forEach(platform => {
+    platform.draw();
+  });
+
   player.update();
 
   if (keys.rightKey.pressed && player.position.x < 400) {
@@ -100,8 +113,47 @@ const animate = () => {
       player.velocity.x = -5;
   } else {
     player.velocity.x = 0;
+  
+
+  if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
+    platforms.forEach((platform) => {
+      platform.position.x -= 5;
+    });
+  } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
+    platforms.forEach((platform) => {
+      platform.position.x += 5;
+    });
   }
-};
+}
+
+platforms.forEach((platform) => {
+  const collisionDetectionRules = [
+    player.position.y + player.height <= platform.position.y,
+    player.position.y + player.height + player.velocity.y >= platform.position.y,
+    player.position.x >= platform.position.x - player.width / 2,
+    player.position.x <=
+      platform.position.x + platform.width - player.width / 3,
+  ];
+
+  if (collisionDetectionRules.every((rule) => rule)) {
+    player.velocity.y = 0;
+    return;
+  }
+
+  const platformDetectionRules = [
+    player.position.x >= platform.position.x - player.width / 2,
+    player.position.x <=
+      platform.position.x + platform.width - player.width / 3,
+    player.position.y + player.height >= platform.position.y,
+    player.position.y <= platform.position.y + platform.height,
+  ];
+
+  if (platformDetectionRules.every(rule => rule)) {
+    player.position.y = platform.position.y + player.height;
+    player.velocity.y = gravity;
+  };
+});
+}
 
 const keys = {
   rightKey: {
